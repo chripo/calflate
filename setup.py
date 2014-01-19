a839feffefd084507c7f5568e5b3cc4b56525774
Christoph Polcin <labs@polcin.de>
2014-01-19 10:57:09 +0100

    update description

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(name='calflate',
     version='0.3',
-    description='Simple script to copy or import icalendar events.',
+    description='copy / import icalendar events',
     author='Christoph Polcin',
     author_email='labs@polcin.de',
     url='https://github.com/chripo/calflate',

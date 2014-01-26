8f14c63f29d3efb04b683faa433412d212991bd3
Christoph Polcin <labs@polcin.de>
2014-01-26 10:55:07 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 
+from os.path import join, dirname
 from setuptools import setup
 
 setup(
     name='calflate',
     version='0.3',
-    description='copy / import icalendar events',
+    description='import external calendar events and vcards',
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
     author='Christoph Polcin',
     author_email='labs@polcin.de',
     url='https://github.com/chripo/calflate',
-    license='BSD',
+    license='BSD revised',
     platforms='Any',
     packages=['calflate'],
     provides=['calflate'],
@@ -18,6 +20,10 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
-        'Operating System :: OS Independent'
+        'Operating System :: OS Independent',
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7"
     ],
 )
